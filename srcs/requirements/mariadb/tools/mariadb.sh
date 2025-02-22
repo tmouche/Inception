55408ab75134cd5038f68708a0c24e5254b9a576
Thibaud Mouche <tmouche@z3r11p5.42lyon.fr>
2025-02-22 22:16:10 +0100

    MARIAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,16 +3,20 @@
 echo "Setting up MariaDB..."
 
 # Start MariaDB in the background
-mariadbd --user=mysql --datadir=/var/lib/mariadb --skip-networking &
-sleep 5  # Wait for MariaDB to start
-
-# Run SQL commands using the MariaDB client
-mariadb -u root <<EOF
+rc-service mariadb start
+# sleep 5  # Wait for MariaDB to start
+#  --datadir=/var/lib/mariadb --user=mysql -
+# Run SQL commands using the MariaDB client -u root
+mariadb << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS $DB_USER@'localhost' IDENTIFIED BY "$($DB_PASSWORD)";
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%' IDENTIFIED BY "$($DB_PASSWORD)";
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD("$($DB_ROOT_PASSWORD)");
+CREATE USER IF NOT EXISTS $DB_USER@'localhost' IDENTIFIED BY "$DB_PASSWORD";
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%' IDENTIFIED BY "$DB_PASSWORD";
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD("$DB_ROOT_PASSWORD");
 FLUSH PRIVILEGES;
 EOF
 
+rc-service mariadb stop
 echo "MariaDB setup complete."
+
+exec "$@"
+#Attention les passwords doivent etre "$($PASSWORD)" pour les paths secrets
--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -15,29 +15,28 @@ if [ ! -f "wp-config.php" ]; then
 
     # Create wp-config.php
     wp config create --allow-root \
-        --dbname=${DB_NAME} \
-        --dbuser=${DB_USER} \
-        --dbpass=${DB_PASSWORD} \
-        --dbhost=${DB_HOST} \
+        --dbname=$DB_NAME \
+        --dbuser=$DB_USER \
+        --dbpass=$DB_PASSWORD \
+        --dbhost=mariadb \
 		--path=/usr/share/webapps/
 
     # Install WordPress
     wp core install --allow-root \
-        --url=https://${DOMAIN_NAME} \
-        --title=${WP_TITLE} \
-        --admin_user=${WP_ADMIN_USER} \
-        --admin_password=${WP_ADMIN_PASSWORD} \
-        --admin_email=${WP_ADMIN_EMAIL}
+        --url="https://$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL"
 
     # Create additional user
     wp user create --allow-root \
-        ${WP_USER} ${WP_USER_EMAIL} \
-        --user_pass=${WP_USER_PASSWORD} \
+        "$WP_USER" "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
         --role=author
 
     # Set correct permissions
     chown -R web-data:web-data /usr/share/webapps/wordpress
 fi
 
-/usr/sbin/php-fpm82 -F
-
+#attention les password doivent etre dans secret
