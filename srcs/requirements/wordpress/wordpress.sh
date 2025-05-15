1d6ca326e0bb52706a29abbad2d353eba039efb1
thibaud <thibaud@inception.fr>
2025-05-15 11:45:50 -0400

    finished

--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -5,7 +5,6 @@ cd /usr/share/webapps
 if [ ! -f "wp-config.php" ]; then
 	wp core download --allow-root
 
-    # Create wp-config.php
     wp config create --allow-root \
         --dbname=$DB_NAME \
         --dbuser=$DB_USER \
@@ -13,7 +12,6 @@ if [ ! -f "wp-config.php" ]; then
         --dbhost=mariadb \
 		--path=/usr/share/webapps/
 
-    # Install WordPress
     wp core install --allow-root \
         --url="https://$DOMAIN_NAME" \
         --title="$WP_TITLE" \
@@ -21,13 +19,11 @@ if [ ! -f "wp-config.php" ]; then
         --admin_password="$WP_ADMIN_PASSWORD" \
         --admin_email="$WP_ADMIN_EMAIL"
 
-    # Create additional user
     wp user create --allow-root \
         "$WP_USER" "$WP_USER_EMAIL" \
         --user_pass="$WP_USER_PASSWORD" \
         --role=author
 
-    # Set correct permissions
     chown -R web-data:web-data /usr/share/webapps/wp-content/
 fi
 
